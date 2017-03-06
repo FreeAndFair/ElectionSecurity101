5f2d91e94c2f32916fb173d94cf02a195f9cfbbc
Joseph Kiniry <kiniry@freeandfair.us>
2017-03-06 14:50:40 -0800

    API revision to reflect changes in UserInterface.
    Also contains a first version of action callback to create UI.

--- eclipse-project/src/main/java/us/freeandfair/es101/VotingSystemChoice.java
+++ eclipse-project/src/main/java/us/freeandfair/es101/VotingSystemChoice.java
@@ -14,28 +14,36 @@
 package us.freeandfair.es101;
 
 import org.jmlspecs.annotation.Pure;
+import org.stringtemplate.v4.ST;
 
 import spark.Request;
 import spark.Response;
+import us.freeandfair.es101.util.StringTemplateUtil;
 
 /**
- * @description <description>
- * @explanation <explanation>
- * @bon OPTIONAL_BON_TYPENAME
+ * @description A UI that lets a voter choose which kind of voting system they wish
+ * to use.
  *  
  * @version 1.0
  * @author Joseph Kiniry <kiniry@freeandfair.us>
  * @author Daniel M. Zimmerman <dmz@freeandfair.us>
  */
 public class VotingSystemChoice extends UserInterface {
-  // @todo Add the collection of supported voting systems.
+  /**
+   * Create a new UI for voting system choice for the election.
+   * @param the_election the election in question.
+   */
+  @Pure
+  public VotingSystemChoice(final Election the_election) {
+    super(the_election);
+  }
   
   /* (non-Javadoc)
    * @see us.freeandfair.es101.UserInterface#my_url_schema()
    */
   @Pure @Override
   public String schema() {
-    return "voting_system_choice";
+    return "/voting_system_choice";
   }
 
   /* (non-Javadoc)
@@ -46,9 +54,14 @@ public class VotingSystemChoice extends UserInterface {
     */
   @Override
   public String action(final Request the_request, final Response the_response) {
-    // todo Auto-generated method stub
-    assert false;
-    //@ assert false;
-    return "VotingSystemChoice!";
+    final ST page_template = StringTemplateUtil.loadTemplate("page");
+    page_template.add("enable_results", false);
+    page_template.add("enable_refresh", false);
+    page_template.add("refresh", "15");
+    final ST voting_system_choice_template = 
+        StringTemplateUtil.loadTemplate("voting_system_choice");
+    voting_system_choice_template.add("voting_systems", my_election.my_voting_systems);
+    page_template.add("body", voting_system_choice_template.render());
+    return page_template.render();
   }
 }
