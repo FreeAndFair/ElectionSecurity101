7290a5d1efd1c53473a4ffd84b90552677963620
Joseph Kiniry <kiniry@freeandfair.us>
2017-03-07 16:14:49 -0800

    First version of BallotMarkingDevice class.

--- eclipse-project/src/main/java/us/freeandfair/es101/BallotMarkingDevice.java
+++ eclipse-project/src/main/java/us/freeandfair/es101/BallotMarkingDevice.java
@@ -15,6 +15,8 @@ package us.freeandfair.es101;
 
 import org.jmlspecs.annotation.Pure;
 
+import us.freeandfair.es101.util.StringTemplateUtil;
+
 /**
  * A system which permits voters to indicate their contest choices and then 
  * captures those voter choices on paper ballots.
@@ -24,48 +26,53 @@ import org.jmlspecs.annotation.Pure;
  * @author Daniel M. Zimmerman <dmz@freeandfair.us>
  */
 public class BallotMarkingDevice extends VotingSystem {
-  /* (non-Javadoc)
-   * @see us.freeandfair.es101.UserInterface#schema()
-   */
+
   @Pure @Override
   public String getSchema() {
     return "/ballot-marking-device";
   }
-
-  /* (non-Javadoc)
-   * @see us.freeandfair.es101.VotingSystem#explanationText()
-   */
+  
   @Pure @Override
-  public String getExplanationText() {
-    return "A hand-marked ballot!";
+  public String getAfterVotingText() {
+    return super.getAfterVotingText() +
+        "\nAt least your vote is guaranteed to be filled out correctly!";
   }
 
-  /* (non-Javadoc)
-   * @see us.freeandfair.es101.VotingSystem#isReceiptManipulable()
-   */
+  @Pure @Override
+  public String getExplanationText() {
+    return StringTemplateUtil.loadTemplate("ballot_marking_device").render();
+  }
+
+  @Pure @Override
+  public String getName() {
+    return "Ballot Marking Device";
+  }
+
+  @Pure @Override
+  public String getUsageRegions() {
+    return "This kind of voting system has been experimented with and rejected by " +
+           "numerous governments around the world, but is being used in around two dozen " +
+           "states in the U.S.A. to collect votes from overseas Americans and military " + 
+           "personnel.  And in Alaska, any citizen can vote over the internet!";
+  }
+
+  @Pure @Override
+  public boolean isReceiptGenerated() {
+    return false;
+  }
+
   @Pure @Override
   public boolean isReceiptManipulable() {
     return false;
   }
 
-  /* (non-Javadoc)
-   * @see us.freeandfair.es101.VotingSystem#isVoteManipulable()
-   */
   @Pure @Override
   public boolean isVoteManipulable() {
     return false;
   }
 
-  /* (non-Javadoc)
-   * @see us.freeandfair.es101.VotingSystem#getName()
-   */
   @Pure @Override
-  public String getName() {
-    return "Hand-Marked!";
-  }
-  
-  @Pure @Override
-  public String getUsageRegions() {
-    return "New Hampshire and Idaho";
-  }
+  public boolean isVoteVisible() {
+    return false;
+  }  
 }
